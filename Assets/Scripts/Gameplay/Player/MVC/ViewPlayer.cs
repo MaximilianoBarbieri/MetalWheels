98c25ddb486de1d3dca6afc2966182b4837d0ac6
RodrigoGangoneDavinci <rodrigo.gangone@davinci.edu.ar>
2025-07-17 17:33:31 -0300

    se agregan visuales

--- Assets/Scripts/Gameplay/Player/MVC/ViewPlayer.cs
+++ Assets/Scripts/Gameplay/Player/MVC/ViewPlayer.cs
@@ -4,13 +4,19 @@ public class ViewPlayer : MonoBehaviour
 {
     [SerializeField] private Renderer carRenderer;
     [SerializeField] private GameObject stunFX;
+    [SerializeField] private GameObject damageFX;
     [SerializeField] private GameObject fireFX;
 
     public void PlayStunFX()
     {
         if (stunFX) stunFX.SetActive(true);
     }
-
+    
+    public void PlayDamageFX()
+    {
+        if (damageFX) damageFX.SetActive(true);
+    }
+    
     public void PlayFireFX()
     {
         if (fireFX) fireFX.SetActive(true);
